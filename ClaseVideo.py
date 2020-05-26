941c57999eb0ba83e15dc7234719c0b493e21bc4
Jair Torres <cylon0223@gmail.com>
2020-05-26 14:27:26 -0500

    Se agrego VideoEnArchivo

---
+++ ClaseVideo.py
@@ -0,0 +1,42 @@
+class Video:
+    def __init__(self, IdVideo, Nombre, url, FechaPublicacion):
+        self.IdVideo = IdVideo
+        self.Nombre = Nombre
+        self.url = url
+        self.FechaPublicacion = FechaPublicacion
+    
+    @property 
+    def IdVideo (self):
+        return self.__IdVideo
+    
+    @property
+    def Nombre (self):
+        return self.__Nombre
+    
+    @Nombre.setter
+    def Nombre (self, valor):
+        self.__Nombre = valor
+
+    @property
+    def url (self):
+        return self.__url
+    
+    @url.setter
+    def url (self, valor):
+        self.__url = valor
+    
+    @property
+    def FechaPublicacion (self):
+        return self.__FechaPuclicacion
+    
+    def Informacion (self):
+        print (f"{'ID Video' :<10}{'Nombre' :<20}{'URL' :<10}{'Fecha de Publicacion':<20}")
+        print (f"{self.__IdVideo:<10}{self.__Nombre:<20}{self.__url:<10}{self.__FechaPuclicacion:<20}")
+
+def VideoEnArchivo(self):
+    archivotxt = open ('./archivos/ videos.txt','a', encoding='utf8')
+    archivotxt.write (self.__IdVideo + '|' + self.__Nombre + '|'+ self.__url +'|' + self.__FechaPuclicacion + )
+    archivotxt.close()
+    
+    
+
