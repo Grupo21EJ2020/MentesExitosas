9a0c7ff6469c16c32073a5ffc7672ecb01f85952
Israel Lopez Cardona <isrrael0919@gmai.com>
2020-05-26 18:46:59 -0500

    Funcion eliminar tema

--- src/main.py
+++ src/main.py
@@ -40,11 +40,29 @@ def main():
     if menuInicial == 3:
         submenuTema = int(input("1) Agregar\n2) Borrar\n3) Modificar\n4) Consultar\nRespuesta:  "))
         if submenuTema== 1:
-            idTema = int(input("Cual es el id del tema?  "))
+            idTema = str(input("Cual es el id del tema?  "))
             Nombre = str(input("Cual es el nombre del tema?  "))
             temaNuevo = Tema(idTema,Nombre)
             print(temaNuevo)
             temaNuevo.agregarTema()
+        elif submenuTema == 2:
+            idTema  = str(input("Digita el id del tema que quieres borrar:  "))
+            ListaTemas = []
+            archivoTema = open("Temas.txt", "r")
+            for n in archivoTema:
+                ListaTemas.append(n)
+                if idTema== n[0]:
+                    ListaTemas.remove(n)
+                archivoTema2 = open("Temas2.txt","w")
+                for g in ListaTemas:
+                    archivoTema2.write(g)
+                archivoTema2.close()
+            archivoTema.close()
+            archivoorigen = open("Temas2.txt","r")
+            archivodestino = open("Temas.txt","w")
+            archivodestino.write(archivoorigen.read())
+            archivoorigen.close()
+            archivodestino.close()
             
         elif submenuTema == 4:
             archivoTema = open("Temas.txt","r")
