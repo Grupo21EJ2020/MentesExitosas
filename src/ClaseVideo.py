103a47ad294766e2925d3ee32f6f7fe5333f0c22
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-05-26 15:49:47 -0500

    Modify archivos duplicados

--- src/ClaseVideo.py
+++ src/ClaseVideo.py
@@ -33,6 +33,27 @@ class Video:
         print (f"{'ID Video' :<10}{'Nombre' :<20}{'URL' :<10}{'Fecha de Publicacion':<20}")
         print (f"{self.__IdVideo:<10}{self.__Nombre:<20}{self.__url:<10}{self.__FechaPuclicacion:<20}")
 
+    def VideoEnArchivo(self):
+        archivotxt = open ('./archivos/ videos.txt','a', encoding='utf8')
+        archivotxt.write (self.__IdVideo + '|' + self.__Nombre + '|'+ self.__url +'|' + self.__FechaPuclicacion  )
+        archivotxt.close()
+
+    @classmethod
+    def EliminarVideo(self, archivotxt, IdVideo):
+        listaVideos = []
+        Archivo1 = open(archivotxt, encoding ='utf8')
+        for clave in Archivo1: 
+            listaVideos.append (clave) 
+            if IdVideo == clave[0]:
+                listaVideos.remove(n)
+            OtroArchivo = open (archivotxt,'w', encoding = 'utf8')
+            for g in listaVideos:
+                OtroArchivo.write(g)
+            OtroArchivo.close()
+        Archivo1.close()
+
+
+    
     
     
 
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from curso import Curso
 def main():
     menuInicial = int(input("1) Empleados\n2) Cursos\n3) Temas\n4) Videos\n5) Temas Asignados\n6) Administracion de Videos\nRespuesta:  "))
     if menuInicial == 2:
-        submenuCurso = int(input("1) Agregar\n2) Borrar\n3) Modificar\n 4) Consultar"))
+        submenuCurso = int(input("1) Agregar\n2) Borrar\n3) Modificar\n4) Consultar\nRespuesta:  "))
         if submenuCurso == 1:
             idCurso = str(input("Cual es el id del curso?  "))
             nombre = str(input("Cual es el nombre del curso?  "))
