e30f0c2eb5442ebdd833b88d442b90626a2aa896
Andres Eduardo Delgado Galván <andreshog@gmail.com>
2020-05-26 19:25:42 -0500

    Se eliminaron propiedades innecesarias

--- src/Curso-Tema-Video.py
+++ src/Curso-Tema-Video.py
@@ -1,10 +1,8 @@
 from ClaseVideo import Video
 objetovideo = Video() 
 class CursoTemaVideo:
-    def _init_(self, IdCursoTemaVideo, IdCursotema, IdVideo):
+    def _init_(self, IdCursoTemaVideo):
         self.__IdCursoTemaVideo = IdCursoTemaVideo
-        self.__IdCursotema = IdCursotema
-        self.__IdVideo = IdVideo
     
     @property 
     def IdCursoTemaVideo (self):
@@ -13,23 +11,6 @@ class CursoTemaVideo:
     @IdCursoTemaVideo.setter
     def IdCursoTemaVideo (self, valor):
         self.__IdCursoTemaVideo = valor
-    
-    @property 
-    def IdCursotema (self):
-        return self.__IdCursotema 
-    
-    @IdCursotema.setter
-    def IdCursotema (self, valor):
-        self.__IdCursotema = valor
-
-    @property 
-    def IdVideo (self):
-        return self.__IdVideo 
-    
-    @IdVideo.setter
-    def IdVideo (self, valor):
-        self.__IdVideo = valor
-
 
     def agregarCursoTemaVideo (self):
         archivo = open("CursoTemaVideo", "a")
