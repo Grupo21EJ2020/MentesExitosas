b734d69c9f01f608c81eb4fc65777ae7b7d063bf
Andres Eduardo Delgado Galván <andreshog@gmail.com>
2020-05-26 23:21:50 -0500

    Se agrego una funcion str

--- src/cursoTemaVideo.py
+++ src/cursoTemaVideo.py
@@ -13,7 +13,10 @@ class CursoTemaVideo:
     @idCursoTemaVideo.setter
     def idCursoTemaVideo (self, valor):
         self.__idCursoTemaVideo = valor
-
+    
+    def __str__ (self):
+        return f'{self.__idCursoTemaVideo} "\n" {self.__idCursoTema} "\n" {self.__IdVideo}'
+        
     def agregarCursoTemaVideo (self):
         archivo = open("./archivos/curso_tema_video.txt", "a")
         archivo.write(self.__idCursoTemaVideo + "|" + self.__idCursoTema + "|" + self.__IdVideo + "\n")
